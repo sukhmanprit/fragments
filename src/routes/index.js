c55a4ebf3cac66f895b83a3672834451d871fb9f
sukhmanprit <sukhmanpreet.k@yahoo.com>
2024-11-25 21:32:09 -0500

    Updated health check route to return hostname

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 // src/routes/index.js
 
 const express = require('express');
+const { hostname } = require('os');
 
 // Our authentication middleware
 const { authenticate } = require('../auth');
@@ -24,17 +25,30 @@ router.use(`/v1`, authenticate(),require('./api'));
  * Define a simple health check route. If the server is running
  * we'll respond with a 200 OK.  If not, the server isn't healthy.
  */
+// router.get('/', (req, res) => {
+//   // Client's shouldn't cache this response (always request it fresh)
+//   res.setHeader('Cache-Control', 'no-cache');
+//   // Using createSuccessResponse instead of res.status(200).json()
+//   const response = createSuccessResponse({
+//     author,
+//     // Use your own GitHub URL for this!
+//     githubUrl: 'https://github.com/sukhmanprit/fragments',
+//     version,
+//   });
+//   res.status(200).json(response);
+// });
+
 router.get('/', (req, res) => {
-  // Client's shouldn't cache this response (always request it fresh)
   res.setHeader('Cache-Control', 'no-cache');
-  // Using createSuccessResponse instead of res.status(200).json()
-  const response = createSuccessResponse({
-    author,
-    // Use your own GitHub URL for this!
-    githubUrl: 'https://github.com/sukhmanprit/fragments',
-    version,
-  });
-  res.status(200).json(response);
+  res.status(200).json(
+    createSuccessResponse({
+      author: 'Sukhmanpreet Kaur',
+      githubUrl: 'https://github.com/sukhmanprit/fragments',
+      version,
+      // Include the hostname in the response
+      hostname: hostname(),
+    })
+  );
 });
 
 module.exports = router;
